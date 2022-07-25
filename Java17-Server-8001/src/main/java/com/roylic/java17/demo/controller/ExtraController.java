d73cebfcb5bb00549b7510e749541c9361c30ff2
Roylic <roylic@wokoworks.com>
2022-07-25 13:58:12 +0800

    Correcting the latest Spring application version

--- Java17-Server-8001/src/main/java/com/roylic/java17/demo/controller/ExtraController.java
+++ Java17-Server-8001/src/main/java/com/roylic/java17/demo/controller/ExtraController.java
@@ -1,8 +1,14 @@
 package com.roylic.java17.demo.controller;
 
+import org.springframework.web.service.annotation.HttpExchange;
+
 /**
  * @author Roylic
  * 2022/7/25
  */
+@HttpExchange
 public class ExtraController {
+
+
+
 }
