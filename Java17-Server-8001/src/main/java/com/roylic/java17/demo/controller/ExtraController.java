86642ffede67bce7a7a7bb68d76cc17ae8b9ff22
roylic <lykismekk@outlook.com>
2022-08-01 23:04:16 +0800

    simple commit

--- Java17-Server-8001/src/main/java/com/roylic/java17/demo/controller/ExtraController.java
+++ Java17-Server-8001/src/main/java/com/roylic/java17/demo/controller/ExtraController.java
@@ -1,14 +1,21 @@
 package com.roylic.java17.demo.controller;
 
-import org.springframework.web.service.annotation.HttpExchange;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.service.annotation.PostExchange;
 
 /**
+ * Controller for testing @HttpExchange
+ *
  * @author Roylic
  * 2022/7/25
  */
-@HttpExchange
+@RestController
 public class ExtraController {
 
+    @PostExchange()
+    public String simple_Endpoint() {
+        return null;
+    }
 
 
 }
