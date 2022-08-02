388d959effd61a827e34e82d813a3935c7ba3289
Roylic <roylic@wokoworks.com>
2022-08-02 09:54:12 +0800

    corrected dependencies version & impl simple controller for testing

--- Java17-Server-8001/src/main/java/com/roylic/java17/demo/controller/AsyncController.java
+++ Java17-Server-8001/src/main/java/com/roylic/java17/demo/controller/AsyncController.java
@@ -1,14 +1,19 @@
 package com.roylic.java17.demo.controller;
 
 import com.roylic.java17.demo.service.AsyncCallbackTask;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
+import org.springframework.util.StopWatch;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.CompletableFuture;
 
-@RestController("/async")
+@Slf4j
+@RestController
+@RequestMapping("/async")
 public class AsyncController {
 
     @Autowired
@@ -17,13 +22,19 @@ public class AsyncController {
     @PostMapping("/test")
     @Async("controllerExecutor")
     public CompletableFuture<String> getFutureTest() throws Exception {
+
+        log.debug(">>>> Receive Calling");
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
         CompletableFuture<String> future_1 = asyncCallbackTask.doTaskOneCallback();
         CompletableFuture<String> future_2 = asyncCallbackTask.doTaskTwoCallback();
         CompletableFuture<String> future_3 = asyncCallbackTask.doTaskThreeCallback();
 
         CompletableFuture<Void> voidCompletableFuture = CompletableFuture.allOf(future_1, future_2, future_3);
         voidCompletableFuture.join();
+        stopWatch.stop();
+        log.debug("<<< Response Calling");
 
-        return CompletableFuture.completedFuture("finished");
+        return CompletableFuture.completedFuture("Finished, in time: " + stopWatch.getTotalTimeMillis() + "ms");
     }
 }
