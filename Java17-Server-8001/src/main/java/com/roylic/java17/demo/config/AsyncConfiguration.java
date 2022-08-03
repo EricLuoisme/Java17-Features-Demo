d1cffb20611063204f42bacb2c73e298c19c4e0b
Roylic <roylic@wokoworks.com>
2022-08-03 11:15:43 +0800

    slightly editing

--- Java17-Server-8001/src/main/java/com/roylic/java17/demo/config/AsyncConfiguration.java
+++ Java17-Server-8001/src/main/java/com/roylic/java17/demo/config/AsyncConfiguration.java
@@ -1,5 +1,6 @@
 package com.roylic.java17.demo.config;
 
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
