7cee7bfe4453b97312450ae6c67570c235609415
roylic <lykismekk@outlook.com>
2023-09-16 12:21:44 +0800

    successfully run JDK21 features under Intellij Env

--- Java21-Features-Demo/src/main/java/LoomTest.java
+++ Java21-Features-Demo/src/main/java/LoomTest.java
@@ -1,5 +1,6 @@
 public class LoomTest {
 
+    // need to add '--enable-preview' as additional command line in compiler -> then enable the JDK21 features
     public static void main(String[] args) {
         Thread.startVirtualThread(() -> System.out.println("Hello Fiber")).start();
     }
