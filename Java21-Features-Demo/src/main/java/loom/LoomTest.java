aafd50e9e1cf987c8d29994bcb230846b828154c
roylic <lykismekk@outlook.com>
2023-09-21 22:24:32 +0800

    update officially example for creating a virtual thread -> no more error

--- Java21-Features-Demo/src/main/java/loom/LoomTest.java
+++ Java21-Features-Demo/src/main/java/loom/LoomTest.java
@@ -4,7 +4,9 @@ public class LoomTest {
 
     // need to add '--enable-preview' as additional command line in compiler -> then    enable the JDK21 features
     // or set -> 21 in Target bytecode version setting in compiler
-    public static void main(String[] args) {
-        Thread.startVirtualThread(() -> System.out.println("Hello Fiber")).start();
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread = Thread.ofVirtual()
+                .start(() -> System.out.println("This one is the correct using"));
+        thread.join();
     }
 }
