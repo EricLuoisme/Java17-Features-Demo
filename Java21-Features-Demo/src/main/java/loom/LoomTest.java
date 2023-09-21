bcf276c82c92c1239e76fbb1403ebb4a80fdd65a
roylic <roylic@wokoworks.com>
2023-09-21 17:55:44 +0800

    cmt for using Loom on JDK21

--- Java21-Features-Demo/src/main/java/loom/LoomTest.java
+++ Java21-Features-Demo/src/main/java/loom/LoomTest.java
@@ -3,6 +3,7 @@ package loom;
 public class LoomTest {
 
     // need to add '--enable-preview' as additional command line in compiler -> then    enable the JDK21 features
+    // or set -> 21 in Target bytecode version setting in compiler
     public static void main(String[] args) {
         Thread.startVirtualThread(() -> System.out.println("Hello Fiber")).start();
     }
