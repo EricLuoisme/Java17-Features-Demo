7bd8fc2bdf6c64ef4576e683a7dd44668201b1fc
roylic <lykismekk@outlook.com>
2023-09-21 22:56:48 +0800

    add simple virtual thread builder

---
+++ Java21-Features-Demo/src/main/java/virtualThread/VirtualThreadBuilderTest.java
@@ -0,0 +1,18 @@
+package virtualThread;
+
+public class VirtualThreadBuilderTest {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        // use Virtual-Thread builder
+        Thread.Builder.OfVirtual builder = Thread.ofVirtual().name("virtual-worker-", 0);
+        Runnable task = () ->
+                System.out.println("Thread ID: " + Thread.currentThread().threadId());
+
+        Thread t1 = builder.start(task);
+        t1.join();
+
+        Thread t2 = builder.start(task);
+        t2.join();
+    }
+}
