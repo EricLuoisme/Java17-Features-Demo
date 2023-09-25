af622f7af576c4a0c8ea17a774a78d9657886e97
roylic <roylic@wokoworks.com>
2023-09-25 11:34:02 +0800

    add a more sufficient use for the JDK21

--- Java21-Features-Demo/src/main/java/virtualThread/VTExecutorTest.java
+++ Java21-Features-Demo/src/main/java/virtualThread/VTExecutorTest.java
@@ -1,12 +1,19 @@
 package virtualThread;
 
+import java.time.Duration;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.stream.IntStream;
 
 public class VTExecutorTest {
 
     public static void main(String[] args) throws InterruptedException {
+        simpleUsing();
+        intUsing();
+    }
+
+    private static void simpleUsing() {
         // use virtual thread task executor
         try (ExecutorService ownExecutor = Executors.newVirtualThreadPerTaskExecutor()) {
             Future<?> future = ownExecutor.submit(() -> System.out.println("Running Thread"));
@@ -15,5 +22,25 @@ public class VTExecutorTest {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    private static void intUsing() {
+        try (
+                var virtualEx = Executors.newVirtualThreadPerTaskExecutor()
+        ) {
+            IntStream.range(0, 10_000).forEach(i ->
+                    virtualEx.submit(() -> {
+                        try {
+                            Thread.sleep(Duration.ofSeconds(1));
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        System.out.println("Finished");
+                        return i;
+                    }));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        // executor.close() is called implicitly, and waits
     }
 }
