b1b382fdb5e8d022c78a7185139d7bae04d2e41c
roylic <roylic@wokoworks.com>
2023-09-25 17:44:33 +0800

    simple string template on Java

---
+++ Java21-Features-Demo/src/main/java/strTemplate/StringTemplate.java
@@ -0,0 +1,12 @@
+package strTemplate;
+
+/**
+ * @author Roylic
+ * 2023/9/25
+ */
+public class StringTemplate {
+    public static void main(String[] args) {
+        String name = "Tom Cruse";
+        System.out.println(STR. "Geetings \{ name }" );
+    }
+}
