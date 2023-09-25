2f5bf026e46c3d265d390a58523afd8ed18a3273
roylic <roylic@wokoworks.com>
2023-09-25 12:02:28 +0800

    add taskScope feature -> on combine result all together

---
+++ Java21-Features-Demo/src/main/java/concurrency/StructuredConcurrency.java
@@ -0,0 +1,64 @@
+package concurrency;
+
+import java.util.concurrent.StructuredTaskScope;
+import java.util.function.Supplier;
+
+/**
+ * StructuredTaskScope: provide a hierarchy on tasks by using VirtualThread
+ *
+ * @author Roylic
+ * 2023/9/25
+ */
+public class StructuredConcurrency {
+
+    public static void main(String[] args) {
+
+        // when we need multiple time-consuming task's result and combine them together to form the response
+        taskScope_combineResults();
+
+        // when we only need one of the result could form the response
+    }
+
+
+    private static void taskScope_combineResults() {
+        try (
+                // the virtualThreadPool will be shut down when it met failure
+                var scope = new StructuredTaskScope.ShutdownOnFailure()
+        ) {
+            // split the task -> by using scope.fork()
+            // originally as type: StructuredTaskScope.Subtask<Integer>
+            Supplier<Integer> accountDetailFuture = scope.fork(() -> getAccountDetails(10086));
+            Supplier<String> linkedAccountsFuture = scope.fork(() -> fetchLinkedAccounts(10086));
+            Supplier<String> userDetailsFuture = scope.fork(() -> fetchUserDetails(10086));
+
+            // join all subtasks
+            scope.join();
+            scope.throwIfFailed(e -> new RuntimeException("Exception")); // handle error when subtask fails
+
+            // return
+            Integer accountDetail = accountDetailFuture.get();
+            String linkedAccounts = linkedAccountsFuture.get();
+            String userDetails = userDetailsFuture.get();
+            System.out.println(accountDetail);
+            System.out.println(linkedAccounts);
+            System.out.println(userDetails);
+
+        } catch (InterruptedException e) {
+            // populate the interruption
+            Thread.currentThread().interrupt();
+        }
+    }
+
+
+    public static Integer getAccountDetails(int id) {
+        return id + 10;
+    }
+
+    public static String fetchLinkedAccounts(int id) {
+        return id + "_id";
+    }
+
+    public static String fetchUserDetails(int id) {
+        return id + "_name";
+    }
+}
