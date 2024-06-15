53ec4957ad16a845b2796396624a877b449cb026
EricLuoisme <35546172+EricLuoisme@users.noreply.github.com>
2024-06-15 16:34:18 +0800

    build: execute correction on application

---
+++ SpringBoot3-Kafka-Logging/src/main/java/demo/SpringKafkaLoggingApplication.java
@@ -0,0 +1,11 @@
+package demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKafkaLoggingApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringKafkaLoggingApplication.class);
+    }
+}
